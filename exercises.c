708ee6ff4d0199e2988d7999a1d508c18e3be72b
Your Name <you@example.com>
2024-03-21 20:15:06 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -58,7 +58,13 @@ Crea una función que reciba una lista de enteros (int*) y
 retorne la suma de sus elementos.
 */
 int sumaLista(List *L) {
-   return 0;
+   int suma = 0;
+   int *elemento = head(L);
+   while (elemento != NULL)
+     {
+       suma += *elemento;
+     }
+   return suma;
 }
 
 /*
